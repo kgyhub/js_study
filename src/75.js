47fa7135abc9a6ebf6a7cc25ef6d8b048dfb52ca
김가영 <gykim@neowiz.com>
2022-04-21 12:42:43 +0900

    74번까지 + 83

--- src/75.js
+++ src/75.js
@@ -1,13 +1,6 @@
-// 사용자에게 대문자로 메시지를 입력하라고 요청한다.
-// 만약 메시지 에 소문자가 있다면 모두 대문자로 입력할 때까지 계속해서 다시 입력하라고 요청한다.
+// 세자리 숫자 네 개가 담긴 배열을 생성한다.
+// 배열의 각 항목을 한 줄에 하나씩 출력하여 사용자에게 표시한다.
+// 사용자에게 세 자리의 숫자를 입력하라고 요청한다.
+// 만약 입력한 숫자가 배열에 있는 숫자들 중 하나라면 배열에 그 숫자가 위치한 인덱스를 출력하라.
+// 그렇지 않다면 "That is not in the list"라는 메시지를 출력하라.
 import { question } from 'readline-sync';
-
-let stat = true;
-while (stat) {
-  const upperCase = question('대문자 메시지를 입력하세요. : ');
-  const checkCase = upperCase.toUpperCase();
-  if (upperCase === checkCase) {
-    //
-    stat = false;
-  }
-}
